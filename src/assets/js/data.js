1e0a83f81ca61f0fdfe56081c7ae552061149c58
Neendu Wickremasinghe <neendu.wickremasinghe@gmail.com>
2025-07-16 03:18:32 +0530

    feat: make a review list using JS and make swiper responsive

---
+++ src/assets/js/data.js
@@ -0,0 +1,62 @@
+import images from "./images";
+
+let reviews = [
+  {
+    name: "Dr. Niroth Samarawickrama",
+    position: "Founder of iCET",
+    image: images.niroth,
+    review:
+      "Anjitha has consistently demonstrated strong academic performance and an eagerness to learn. His dedication to both his studies and practical work has been impressive.",
+  },
+  {
+    name: "Dewmal Hadapangoda",
+    position: "Senior Lecturer at iCET | CEO & Founder of SYIGEN LTD",
+    image: images.dewmal,
+    review:
+      "Anjitha demonstrated strong problem-solving skills during his final project, delivering clear and effective solutions with great attention to detail.",
+  },
+  {
+    name: "Frank Cronemberg",
+    position: "MSc.(SE), MBCS(UK), RITTech",
+    image: images.frank,
+    review:
+      "Anjitha handled tight deadlines with professionalism. His technical skills and composed approach to challenges were truly admirable.",
+  },
+  {
+    name: "Sharadha Marasinghe",
+    position: "Assistant Lecturer @iCET",
+    image: images.sharadha,
+    review:
+      "Anjitha consistently showcased creative thinking and strong technical skills, delivering high-quality work in every project.",
+  },
+  {
+    name: "Kushani Dhanushika",
+    position: "Software Engineer | Academic Leader",
+    image: images.kushani,
+    review:
+      "Working with Anjitha was a great experience. His clean code, thorough documentation, and timely delivery were impressive.",
+  },
+  {
+    name: "Nethma Kannangara",
+    position: "Assistant Lecturer @iCET",
+    image: images.nethma,
+    review:
+      "Anjitha displayed excellent analytical skills, breaking down complex issues into manageable parts while keeping the overall goal in mind.",
+  },
+  {
+    name: "Sachin Samarawickrama",
+    position: "Assistant Lecturer @iCET",
+    image: images.sachin,
+    review:
+      "Anjitha’s commitment to improvement was remarkable. He actively sought feedback and quickly applied it to enhance his work.",
+  },
+  {
+    name: "Pawani Alahakoon",
+    position: "Student Councillor",
+    image: images.pawani,
+    review:
+      "Anjitha’s time management and positive attitude helped him balance academics and extracurricular activities, making him a great role model.",
+  },
+];
+
+export { reviews };
---
+++ src/assets/js/images.js
@@ -0,0 +1,23 @@
+import niroth from "../images/avatars/Niroth Samarawickrama.jpeg";
+import kushani from "../images/avatars/Kushani Dhanushika.jpeg";
+import frank from "../images/avatars/Frank Cronemberg.jpeg";
+import dewmal from "../images/avatars/Dewmal Handapangoda.jpeg";
+import sachin from "../images/avatars/Sachin Samarawickrama.jpeg";
+import sharadha from "../images/avatars/Sharada Marasinghe.jpeg";
+import nethma from "../images/avatars/Nethma Kannangara.jpeg";
+import pawani from "../images/avatars/Pawani Alahakoon.jpeg";
+import prathiba from "../images/avatars/Prathibha Weerasinghe.jpeg";
+
+const images = {
+  niroth,
+  kushani,
+  frank,
+  dewmal,
+  sachin,
+  sharadha,
+  nethma,
+  pawani,
+  prathiba,
+};
+
+export default images;
